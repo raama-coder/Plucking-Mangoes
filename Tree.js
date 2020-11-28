50f8e5fcf0afc99396e4e53eacd964ac18142f33
raama-coder <72999673+raama-coder@users.noreply.github.com>
2020-11-28 09:51:37 -0500

    Update Tree.js

--- Tree.js
+++ Tree.js
@@ -3,7 +3,7 @@ class Tree{
       var options = {
           isStatic: true
       }
-      this.image=loadImage("Sprites/tree.png")
+      this.image=loadImage("tree.png")
       this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
