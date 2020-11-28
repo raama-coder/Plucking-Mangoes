8b620966732784ce9f84522cebf15206e2736b47
raama-coder <72999673+raama-coder@users.noreply.github.com>
2020-11-28 09:51:07 -0500

    Update Rock.js

--- Rock.js
+++ Rock.js
@@ -10,7 +10,7 @@ class Stone{
     this.x=x
     this.y=y
     this.r=r
-    this.image=loadImage("Sprites/stone.png")
+    this.image=loadImage("stone.png")
     this.body=Bodies.circle(this.x, this.y, this.r/2, options);
     World.add(world, this.body)
     }
